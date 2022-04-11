557a47b10231aa360ae597ea4bca2ad617c7395d
khangnguyen <thinhkhang97@gmail.com>
2022-04-12 05:51:06 +0700

    Refactor circle slider

--- src/lessons/CircleSlider.tsx
+++ src/lessons/CircleSlider.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Dimensions, PixelRatio, StyleSheet, View } from "react-native";
+import { Dimensions, StyleSheet, View } from "react-native";
 import { PanGestureHandler } from "react-native-gesture-handler";
 import Animated, {
 	useAnimatedGestureHandler,
@@ -10,74 +10,96 @@ import Animated, {
 import { clamp } from "react-native-redash";
 import Svg, { Circle } from "react-native-svg";
 
-type Props = {};
-
-const { width, height } = Dimensions.get("window");
-
-const size = width - 32;
-const r = PixelRatio.roundToNearestPixel(size / 2);
-const x0 = r;
-const y0 = r;
-const strokeWidth = 40;
-
 const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 
+type Props = {};
+interface Point {
+	x: number;
+	y: number;
+}
+
+const { width } = Dimensions.get("window");
+const r = width / 2 - 24;
+const strokeWidth = 40;
+const cursorWidth = strokeWidth;
+const PI = Math.PI;
+
 export const CircleSlider: React.FC<Props> = () => {
-	const alpha = useSharedValue((Math.PI * 3) / 2);
+	const alpha = useSharedValue(PI / 3);
+	const center = {
+		x: r,
+		y: r,
+	};
 	const radius = r - strokeWidth / 2;
-	const circleLength = radius * Math.PI * 2;
-
-	const props = useAnimatedProps(() => {
-		return {
-			strokeDashoffset: (circleLength * alpha.value) / (Math.PI * 2),
-		};
-	});
-
 	return (
-		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+		<View style={styles.container}>
 			<View style={styles.content}>
 				<Svg style={StyleSheet.absoluteFill}>
-					<AnimatedCircle
-						animatedProps={props}
-						cx={x0}
-						cy={y0}
+					<Circle
+						cx={r}
+						cy={r}
 						r={radius}
 						strokeWidth={strokeWidth}
 						stroke={"blue"}
-						strokeDasharray={`${circleLength} ${circleLength}`}
 					/>
+					<CircleProgress radius={radius} alpha={alpha} />
 				</Svg>
-				<Cursor r={r - strokeWidth / 2} alpha={alpha} />
+				<Cursor center={center} alpha={alpha} radius={radius} />
 			</View>
 		</View>
 	);
 };
 
-type CursorProps = {
-	r: number;
+type CircleProgressProps = {
+	radius: number;
 	alpha: Animated.SharedValue<number>;
 };
-export const Cursor: React.FC<CursorProps> = ({ r, alpha }) => {
-	const center = { x: r, y: r };
 
+const CircleProgress: React.FC<CircleProgressProps> = ({ radius, alpha }) => {
+	const circleLength = 2 * PI * radius;
+	const props = useAnimatedProps(() => {
+		const length = alpha.value * radius;
+		return {
+			strokeDashoffset: length - circleLength,
+		};
+	});
+	return (
+		<AnimatedCircle
+			animatedProps={props}
+			origin={`${r}, ${r}`}
+			cx={r}
+			cy={r}
+			r={radius}
+			strokeWidth={strokeWidth}
+			stroke={"green"}
+			strokeDasharray={[circleLength]}
+		/>
+	);
+};
+
+type CursorProps = {
+	alpha: Animated.SharedValue<number>;
+	center: Point;
+	radius: number;
+};
+
+const Cursor: React.FC<CursorProps> = ({ alpha, center, radius }) => {
 	const onGestureEvent = useAnimatedGestureHandler({
 		onStart: (_, ctx) => {
-			const _translation = polar2Canvas(center, r, alpha);
-			ctx.offsetX = _translation.x;
-			ctx.offsetY = _translation.y;
+			ctx.offset = polar2canvas(center, radius, alpha.value);
 		},
 		onActive: (event, ctx) => {
-			const x = ctx.offsetX + event.translationX;
-			const _y = ctx.offsetY + event.translationY;
+			const x = event.translationX + ctx.offset.x;
+			const _y = event.translationY + ctx.offset.y;
 			let y = _y;
 			if (x < center.x) {
 				y = _y;
-			} else if (alpha.value < Math.PI) {
+			} else if (alpha.value < PI) {
 				y = clamp(_y, 0, center.y - 0.01);
 			} else {
-				y = clamp(_y, center.y + 0.01, center.y + r);
+				y = clamp(_y, center.y + 0.01, center.y + radius);
 			}
-			alpha.value = canvas2Polar(center, {
+			alpha.value = freePoint2Polar(center, {
 				x,
 				y,
 			});
@@ -85,66 +107,59 @@ export const Cursor: React.FC<CursorProps> = ({ r, alpha }) => {
 	});
 
 	const animatedStyles = useAnimatedStyle(() => {
-		const translation = polar2Canvas(center, r, alpha);
+		const translation = polar2canvas(center, radius, alpha.value);
 		return {
-			transform: [{ translateX: translation.x }, { translateY: translation.y }],
+			transform: [
+				{ translateX: translation.x - cursorWidth / 2 },
+				{ translateY: translation.y - cursorWidth / 2 },
+			],
 		};
 	});
-
 	return (
-		<PanGestureHandler {...{ onGestureEvent }}>
+		<PanGestureHandler onGestureEvent={onGestureEvent}>
 			<Animated.View style={[styles.cursor, animatedStyles]} />
 		</PanGestureHandler>
 	);
 };
 
-const polar2Canvas = (
-	center: { x: number; y: number },
-	r: number,
-	alpha: Animated.SharedValue<number>
-) => {
+const polar2canvas = (center: Point, radius: number, alpha: number) => {
 	"worklet";
 	return {
-		x: center.x + r * Math.cos(alpha.value),
-		y: center.y - r * Math.sin(alpha.value),
+		x: center.x + radius * Math.cos(alpha),
+		y: center.y - radius * Math.sin(alpha),
 	};
 };
 
-const getDistance = (
-	point1: { x: number; y: number },
-	point2: { x: number; y: number }
-) => {
+const distance = (point1: Point, point2: Point) => {
 	"worklet";
-	const signal = point1.y - point2.y >= 0 ? 1 : -1;
-	return (
-		signal *
-		Math.sqrt(
-			Math.pow(point1.x - point2.x, 2) + Math.pow(point1.y - point2.y, 2)
-		)
+	return Math.sqrt(
+		Math.pow(point1.x - point2.x, 2) + Math.pow(point1.y - point2.y, 2)
 	);
 };
 
-const canvas2Polar = (
-	center: { x: number; y: number },
-	point: { x: number; y: number }
-) => {
+const freePoint2Polar = (center: Point, point: Point) => {
 	"worklet";
-	const distance = getDistance(center, point);
-	if (distance < 0) {
-		return Math.PI + Math.acos((point.x - center.x) / distance);
+	const d = distance(center, point);
+	if (center.y < point.y) {
+		return PI + Math.acos((center.x - point.x) / d);
 	}
-	return Math.acos((point.x - center.x) / distance);
+	return Math.acos((point.x - center.x) / d);
 };
 
 const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
 	content: {
 		width: 2 * r,
 		height: 2 * r,
 	},
 	cursor: {
-		width: strokeWidth,
-		height: strokeWidth,
-		backgroundColor: "green",
-		borderRadius: strokeWidth / 2,
+		width: cursorWidth,
+		height: cursorWidth,
+		borderRadius: cursorWidth / 2,
+		backgroundColor: "red",
 	},
 });
